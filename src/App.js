ed7ba578507ad5fd72423fd4b03e3c1feb095751
Sajith3992 <k3992.sajith@gmail.com>
2022-11-09 09:14:44 +0530

    fix error app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-
+import React from 'react';
 import './App.css';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import{BrowserRouter, Switch, Route} from 'react-router-dom';
+import AddEdit from "./pages/AddEdit";
 import Home from './pages/Home';
-import AddEdit from './pages/AddEdit';
 import View from './pages/View';
 import About from './pages/About';
 function App() {
   return (
     <BrowserRouter>
-       <div className="App">
-        <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route path='/add' component={AddEdit}/>
-            <Route path='/update/:id' component={AddEdit}/>
-            <Route path='/view/:id' component={View}/>
-            <Route path='/about' component={About}/>
 
-          </Switch>
-       </div>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/add' component ={AddEdit}/>
+          <Route path='/update/: id' component ={AddEdit}/>
+          <Route path='/view/: id' component ={View}/>
+          <Route path='/about' component ={About}/>
+        </Switch>
+      
+      </div>
     </BrowserRouter>
+   
+   
  
   );
 }
