3e7d65c209c5588f5fd6b5f70a0cd3015a86ccf4
Sajith3992 <k3992.sajith@gmail.com>
2022-11-09 11:48:03 +0530

    fix handle chnge

--- src/pages/AddEdit.js
+++ src/pages/AddEdit.js
@@ -12,9 +12,25 @@ const initialState = {
 }
 
 const AddEdit = () => {
+  const [state, setState] = useState(initialState);
+  const [data , setData] =  useState({});
+
+  const {name, email, contact} = state;
+  const handleInputChange = () => {};
+const handleSubmit =() =>{};
   return (
-    <div>
-      <h2>Add AddEdit</h2>
+    <div style={{margintop:"100px"}}>
+      <form style={{margin:"auto",
+       padding:"15px",
+       maxwidth:"400px",
+       alignContent:"center"}}
+       onSubmit={handleSubmit}
+       >
+        <label htmlFor="name">Name</label>
+        <input type="text"id="name" name="name"placeholder="Your Name.."value={name}
+        onChange={handleInputChange}/>
+       </form>
+      
     </div>
   )
 }
