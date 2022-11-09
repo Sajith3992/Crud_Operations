1413676069816995239c6d4038b35c92d5f332f9
Sajith3992 <k3992.sajith@gmail.com>
2022-11-09 10:07:17 +0530

    fix erro in css

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import View from './pages/View';
 import About from './pages/About';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
+
 function App() {
   return (
     <BrowserRouter>
-  
+    
       <div className="App">
+      <Header/>
         <ToastContainer position='top-center'/>
         <Switch>
           <Route exact path='/' component={Home}/>
