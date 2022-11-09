9d7f269b72b6e654a66151f23011581464afc694
Sajith3992 <k3992.sajith@gmail.com>
2022-11-09 08:09:33 +0530

    add path

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Home from './pages/Home';
 import AddEdit from './pages/AddEdit';
+import View from './pages/View';
+import About from './pages/About';
 function App() {
   return (
     <BrowserRouter>
        <div className="App">
+        <Switch>
             <Route exact path='/' component={Home}/>
             <Route path='/add' component={AddEdit}/>
             <Route path='/update/:id' component={AddEdit}/>
-            <Route path='/add' component={AddEdit}/>
+            <Route path='/view/:id' component={View}/>
+            <Route path='/about' component={About}/>
 
+          </Switch>
        </div>
     </BrowserRouter>
  
