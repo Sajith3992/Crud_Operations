5d3509715d34287872ef1371d07a0c39078e8a24
Sajith3992 <k3992.sajith@gmail.com>
2022-11-10 11:14:07 +0530

    import hooks and css file

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react';
+import{useLocation, Link} from 'react-router-dom';
+import firebaseApp from '../firebase';
+import "./Search.css"
+
 
 const Search = () => {
   return (
