05db489873243e0769a75d3d0cdd679c2d60f325
Sajith3992 <k3992.sajith@gmail.com>
2022-11-10 06:30:16 +0530

    create useState

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
-import React,{useEffect, useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 import firebaseApp from '../firebase';
 import { Link } from 'react-router-dom';
 import "./Home.css";
 
 const Home = () => {
+  const [data, setData] = useState({});
   return (
     <div>
       <h2>Home</h2>
