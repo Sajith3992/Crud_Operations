c8ad9f9e28030ca2ee14929e3d33fd224a201cc3
Sajith3992 <k3992.sajith@gmail.com>
2022-11-10 09:06:40 +0530

    import

--- src/pages/View.js
+++ src/pages/View.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react';
+import firebaseApp from '../firebase';
+import{useHistory , useParams, Link} from 'react-router-dom';
+
 
 const View = () => {
   return (
