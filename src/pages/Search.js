ba786242ea039ade5c2b3c89211b0e874502fb4f
Sajith3992 <k3992.sajith@gmail.com>
2022-11-10 11:50:39 +0530

    add table tag

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,3 +1,4 @@
+import { Query } from 'firebase/firestore';
 import React,{useEffect, useState} from 'react';
 import{useLocation, Link} from 'react-router-dom';
 import firebaseApp from '../firebase';
@@ -31,33 +32,35 @@ const Search = () => {
     <>
        <div style={{margintop: "100px"}}>
           {Object.keys(data).length === 0?(
-            
+            <h2> No Search Found with that Name : {query.get("name")}</h2>
+          ):(
+            <table className='style-table'>
+            <thead>
+                <tr>
+                  <th style={{textAlign:"center"}}>No. </th>
+                  <th style={{textAlign:"center"}}>Name </th>
+                  <th style={{textAlign:"center"}}>Email </th>
+                  <th style={{textAlign:"center"}}>Contact </th>
+                 
+                </tr>
+            </thead>
+            {Object.keys(data).map((id, index) =>{
+              return(
+               <tr key={id}>
+               <th scope='row'> {index + 1}</th>
+               <td>{data[id].name}</td>
+               <td>{data[id].email}</td>
+               <td>{data[id].contact}</td>
+         
+               </tr>
+            );
+            })}
+            <tbody>
+    
+            </tbody>
+          </table> 
           )}
-      <table className='style-table'>
-        <thead>
-            <tr>
-              <th style={{textAlign:"center"}}>No. </th>
-              <th style={{textAlign:"center"}}>Name </th>
-              <th style={{textAlign:"center"}}>Email </th>
-              <th style={{textAlign:"center"}}>Contact </th>
-             
-            </tr>
-        </thead>
-        {Object.keys(data).map((id, index) =>{
-          return(
-           <tr key={id}>
-           <th scope='row'> {index + 1}</th>
-           <td>{data[id].name}</td>
-           <td>{data[id].email}</td>
-           <td>{data[id].contact}</td>
-     
-           </tr>
-        );
-        })}
-        <tbody>
-
-        </tbody>
-      </table>
+      
     </div>
     </>
   );
