b97a6dbb721b80f3b13b0c65d322e524b27c373b
Sajith3992 <k3992.sajith@gmail.com>
2022-11-10 11:38:10 +0530

    copy home table file in js and past serch h2 tag

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -66,7 +66,7 @@ const Home = () => {
         </tbody>
       </table>
     </div>
-  )
+  );
 }
 
 export default Home
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -28,9 +28,43 @@ const Search = () => {
         } )
     }
   return (
-    <div>
-      <h2> Search</h2>
+    <>
+       <div style={{margintop: "100px"}}>
+      <table className='style-table'>
+        <thead>
+            <tr>
+              <th style={{textAlign:"center"}}>No. </th>
+              <th style={{textAlign:"center"}}>Name </th>
+              <th style={{textAlign:"center"}}>Email </th>
+              <th style={{textAlign:"center"}}>Contact </th>
+              <th style={{textAlign:"center"}}>Action </th>
+            </tr>
+        </thead>
+        {Object.keys(data).map((id, index) =>{
+          return(
+           <tr key={id}>
+           <th scope='row'> {index + 1}</th>
+           <td>{data[id].name}</td>
+           <td>{data[id].email}</td>
+           <td>{data[id].contact}</td>
+           <td>
+           <Link to={`/update/${id}`}>
+           <button className="btn btn-edit">Edit</button>
+           </Link>
+           <button className="btn btn-delete" onClick={()=>onDelete(id)}>Delete</button>
+           <Link to={`/view/${id}`}>
+           <button className="btn btn-view">View</button>
+           </Link>
+           </td>
+           </tr>
+        );
+        })};
+        <tbody>
+
+        </tbody>
+      </table>
     </div>
+    </>
   )
 }
 
